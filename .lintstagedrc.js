bc7ff6c666167cf315a38dfbd763d9c91a48753f
Patrickresplandes <patrickresplandes87@gmail.com>
2024-11-28 14:38:16 -0300

    update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.map((f) => `"${f}"`).join(' ')}`,
-    `npm run lint --fix ${filenames.map((f) => `"${f}"`).join(' ')}`,
+    //`npm run lint --fix ${filenames.map((f) => `"${f}"`).join('" "')}`,
     `npm test -- --findRelatedTests ${filenames.map((f) => `"${f}"`).join(' ')}`
   ]
 }
